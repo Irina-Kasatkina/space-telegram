9b3b52d69010b4eba153036264d3efb5f98b70ee
Irina Kasatkina <kasatkina235@gmail.com>
2022-08-23 13:20:08 +0300

    publish text to Telegram channel

---
+++ bot.py
@@ -0,0 +1,8 @@
+﻿import telegram
+
+bot = telegram.Bot(token='5339286582:AAHy4w-c0KZZ7ntZ6HF_gpwDGv1OGQSUVTk')
+# print(bot.get_me())
+
+# updates = bot.get_updates()
+
+bot.send_message(text='какой-нибудь текст в канал', chat_id='@spacex_and_nasa_photo_channel')
