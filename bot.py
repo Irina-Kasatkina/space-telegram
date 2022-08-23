5147590dc835b68dee91885662903e6d000b0a5c
Irina Kasatkina <kasatkina235@gmail.com>
2022-08-23 13:31:09 +0300

    publish photo to Telegram channel

--- bot.py
+++ bot.py
@@ -5,4 +5,5 @@ bot = telegram.Bot(token='5339286582:AAHy4w-c0KZZ7ntZ6HF_gpwDGv1OGQSUVTk')
 
 # updates = bot.get_updates()
 
-bot.send_message(text='какой-нибудь текст в канал', chat_id='@spacex_and_nasa_photo_channel')
+# bot.send_message(chat_id='@spacex_and_nasa_photo_channel', text='какой-нибудь текст в канал')
+bot.send_document(chat_id='@spacex_and_nasa_photo_channel', document=open('images/nasa_apod/nasa_apod_1.gif', 'rb'))
